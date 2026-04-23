a817643c80b19849711363ee52f67e2566f30b9e
LukasCea <lukas.ceasandoval.7e9@itb.cat>
2026-04-23 11:38:56 +0200

    build: initiation of the database and the database file

---
+++ fase4/database/dbInit.php
@@ -0,0 +1,13 @@
+<?php
+
+$db = new SQLite3(__DIR__ . '/database_login.db');
+
+$db->exec("CREATE TABLE IF NOT EXISTS users (
+    id_user INTEGER PRIMARY KEY AUTOINCREMENT, 
+    username TEXT, 
+    password TEXT
+    )");
+
+$db->exec("INSERT OR IGNORE INTO users (username, password) VALUES ('admin', '123') ");
+
+$db->close();
